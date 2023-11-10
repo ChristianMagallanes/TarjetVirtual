9a9725fb5f19234109883e53fc3c4777ce89c2f2
christian magallanes <dar4del-x@hotmail.com>
2023-11-10 13:58:11 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import RegistroUsuarios from './components/Asistir/Asistir';
 import UsuariosRegistrados from './components/Registros/Registros';
-import "./App.css"
+import "./App.css";
+
+import backgroundMusic from './music.mp3'; // Reemplaza con la ruta correcta de tu canción
+
 function App() {
+  const [showWelcome, setShowWelcome] = useState(true);
+  const [audioElement, setAudioElement] = useState(null);
+
+  useEffect(() => {
+    // Crear el elemento de audio
+    const audio = new Audio(backgroundMusic);
+    audio.loop = true;
+    setAudioElement(audio);
+
+    // Ocultar el mensaje de bienvenida después de 5 segundos
+    const timeout = setTimeout(() => {
+      setShowWelcome(false);
+    }, 5000);
+
+    return () => {
+      // Detener la música de fondo cuando el componente se desmonta
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  const handlePlayButtonClick = () => {
+    // Reproducir la música al hacer clic en el botón
+    if (audioElement) {
+      audioElement.play();
+    }
+  };
+
   return (
     <div className='container'>
+      <CSSTransition
+        in={showWelcome}
+        timeout={300}
+        classNames="welcome"
+        unmountOnExit
+      >
+        <div className='welcome-message'>
+          <div className='welcome-text'>Bienvenido</div>
+          <button onClick={handlePlayButtonClick}>Reproducir</button>
+        </div>
+      </CSSTransition>
       <RegistroUsuarios />
       <UsuariosRegistrados />
     </div>
--- src/components/Asistir/Asistir.jsx
+++ src/components/Asistir/Asistir.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../Firebase/FirebaseConfig';
+import Swal from 'sweetalert2';
 import "./Asistir.css";
-import imagenTarjeta from "../../assets/tarjet.png"
+import imagenTarjeta from "../../assets/tarjet.png";
 
 const RegistroUsuarios = () => {
   const [name, setName] = useState('');
@@ -10,12 +11,35 @@ const RegistroUsuarios = () => {
 
   const handleRegistro = async () => {
     try {
+      if (!name || asistira === null) {
+        // Mostrar SweetAlert indicando que se debe ingresar un nombre y seleccionar una opción
+        Swal.fire({
+          icon: 'warning',
+          title: 'Por favor, complete los campos',
+          text: 'Ingrese su nombre y seleccione una opción de confirmación.',
+        });
+        return;
+      }
+
       // Registra al usuario en Firebase con el nombre y la asistencia
       const docRef = await addDoc(collection(db, 'usuarios'), {
         name: name,
         asistira: asistira,
       });
       console.log('Usuario registrado con ID:', docRef.id);
+
+      // Limpiar el campo de nombre después de la validación
+      setName('');
+
+      // Limpiar el estado de asistira
+      setAsistira(null);
+
+      // Mostrar SweetAlert de agradecimiento
+      Swal.fire({
+        icon: 'success',
+        title: '¡Muchas gracias por confirmar!',
+        text: 'Esperamos verte en nuestro evento.',
+      });
     } catch (error) {
       console.error('Error al registrar el usuario:', error);
     }
@@ -25,37 +49,56 @@ const RegistroUsuarios = () => {
     setAsistira(asistira);
   };
 
+  const handleValidar = () => {
+    // Verificar si se ha ingresado un nombre y se ha seleccionado una opción antes de validar
+    if (name && asistira !== null) {
+      // Lógica para validar
+      handleRegistro();
+    } else {
+      // Mostrar SweetAlert indicando que se debe ingresar un nombre y seleccionar una opción
+      Swal.fire({
+        icon: 'warning',
+        title: 'Por favor, complete los campos',
+        text: 'Ingrese su nombre y seleccione una opción de confirmación.',
+      });
+    }
+  };
+
   return (
-    
     <div className='form'>
-    
       <img
         src={imagenTarjeta}
         alt="Descripción de la imagen"
         className="imagen-tarjeta"
       />
       <div className="tarjet-text">
-      <p className="text">hola soy martina y, quiero invitarte este tanto tanto a mi cumpleaños</p>
-    </div>
+        <p className="text">hola soy martina y, quiero invitarte este tanto tanto a mi cumpleaños</p>
+      </div>
       <div className='confirmar-container'>
-      <h2>Confirmacion</h2>
-      <input className='input'
-        type="text"
-        placeholder="ingresa tu nombre"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <div className='confirmar-btns'>
-        
-      <button className='form-btn' onClick={() => handleAsistencia(true)}>Si asistiré</button>
-        <button className='form-btn' onClick={() => handleAsistencia(false)}>No asistiré</button>
-        <button className='form-btn' onClick={() => handleAsistencia(null)}>Quizás asistiré</button>
-      
-      <button onClick={handleRegistro}>Validar</button>
-      </div>
+        <h2>Confirmacion</h2>
+        <input
+          className='input'
+          type="text"
+          placeholder="ingresa tu nombre"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <div className='confirmar-btns'>
+          <button className={`form-btn yes ${asistira === true ? 'active' : ''}`} onClick={() => handleAsistencia(true)}>
+            Si asistiré
+          </button>
+          <button className={`form-btn no ${asistira === false ? 'active' : ''}`} onClick={() => handleAsistencia(false)}>
+            No asistiré
+          </button>
+          <button className={`form-btn maybe ${asistira === null ? 'active' : ''}`} onClick={() => handleAsistencia(null)}>
+            Quizás asistiré
+          </button>
+          <button className={`form-btn validar-btn ${asistira !== null ? 'active' : ''}`} onClick={handleValidar}>
+            Validar
+          </button>
+        </div>
       </div>
     </div>
-    
   );
 };
 
